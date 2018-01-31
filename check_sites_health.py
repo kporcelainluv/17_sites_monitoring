9090fe07a7e27c072f48c052f839ef2ba55b474a
Ksusha Zhukova <zhukovaksusha@gmail.com>
2018-01-31 18:48:41 +0300

    shortened function

--- check_sites_health.py
+++ check_sites_health.py
@@ -32,27 +32,32 @@ def get_domain_expiration_date(url):
     return website_info.expiration_date
 
 
+def output_info_to_console(links, minimum_days_of_domain_payment, todays_date):
+    for link in links:
+        print(link, end=" ")
+        server_respond = is_server_respond_with_200(link)
+        if server_respond is None:
+            print("failed to connect")
+        else:
+            print(server_respond, end=", ")
+            exp_date = get_domain_expiration_date(link)
+            if exp_date is None:
+                print('The domain does not exist, exiting...')
+            else:
+                days_due_expirency = abs(exp_date - todays_date).days
+                if days_due_expirency <= minimum_days_of_domain_payment:
+                    print("Domain expires in {} days".format(days_due_expirency))
+                else:
+                    print("Domain lasts more than 30 days")
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         text_file = sys.argv[1]
         minimum_days_of_domain_payment = 30
         todays_date = datetime.now()
         links = collect_urls_to_a_list(text_file)
-        for link in links:
-            print(link, end=" ")
-            server_respond = is_server_respond_with_200(link)
-            if server_respond is None:
-                print("failed to connect")
-            else:
-                print(server_respond, end=", ")
-                exp_date = get_domain_expiration_date(link)
-                if exp_date is None:
-                    print('The domain does not exist, exiting...')
-                else:
-                    days_due_expirency = abs(exp_date - todays_date).days
-                    if days_due_expirency <= minimum_days_of_domain_payment :
-                        print("Domain expires in {} days".format(days_due_expirency))
-                    else:
-                        print("Domain lasts more than 30 days")
+        output_info_to_console(links, minimum_days_of_domain_payment, todays_date)
+
     else:
         print("Enter a valid txt file")
