f145216128ed10168b82dddbaf33c848065c5491
Ksusha Zhukova <zhukovaksusha@gmail.com>
2018-02-01 17:27:41 +0300

    update funcs usage

--- check_sites_health.py
+++ check_sites_health.py
@@ -12,10 +12,10 @@ def collect_urls_to_a_list(text_file):
     return list_of_links
 
 
-def is_server_respond_with_200(url):
+def check_server_status_code(url):
     try:
-        req = requests.request('GET', url)
-        return req
+        website_header = requests.head(url)
+        return website_header.status_code
     except requests.ConnectionError:
         return None
 
@@ -32,32 +32,44 @@ def get_domain_expiration_date(url):
     return website_info.expiration_date
 
 
-def output_info_to_console(links, minimum_days_of_domain_payment, todays_date):
-    for link in links:
-        print(link, end=" ")
-        server_respond = is_server_respond_with_200(link)
-        if server_respond is None:
-            print("failed to connect")
+def output_status_and_exp_date_to_console(todays_date,
+                                          ok_status_code,
+                                          minimum_days_of_domain_payment,
+                                          server_respond,
+                                          exp_date):
+    if server_respond == ok_status_code:
+        print("HTTP Status Code 200: The request has succeeded", end=" ")
+    else:
+        print("HTTP Status Code is {}".format(server_respond), end=" ")
+
+    if exp_date is None:
+        print('The domain does not exist, exiting...')
+    else:
+        days_due_expirency = abs(exp_date - todays_date).days
+        if days_due_expirency <= minimum_days_of_domain_payment:
+            print("Domain expires in {} days".format(days_due_expirency))
         else:
-            print(server_respond, end=", ")
-            exp_date = get_domain_expiration_date(link)
-            if exp_date is None:
-                print('The domain does not exist, exiting...')
-            else:
-                days_due_expirency = abs(exp_date - todays_date).days
-                if days_due_expirency <= minimum_days_of_domain_payment:
-                    print("Domain expires in {} days".format(days_due_expirency))
-                else:
-                    print("Domain lasts more than 30 days")
+            print("Domain lasts more than 30 days")
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         text_file = sys.argv[1]
-        minimum_days_of_domain_payment = 30
         todays_date = datetime.now()
         links = collect_urls_to_a_list(text_file)
-        output_info_to_console(links, minimum_days_of_domain_payment, todays_date)
+        ok_status_code = 200
+        minimum_days_of_domain_payment = 30
+        for link in links:
+            print(link, end=" ")
+            server_respond = check_server_status_code(link)
+            if server_respond is None:
+                exit("failed to connect")
+            exp_date = get_domain_expiration_date(link)
+            output_status_and_exp_date_to_console(todays_date,
+                                                  ok_status_code,
+                                                  minimum_days_of_domain_payment,
+                                                  server_respond,
+                                                  exp_date)
 
     else:
         print("Enter a valid txt file")
