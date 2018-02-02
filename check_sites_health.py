332d0b50295fa9398c5b6217cd6cf1234eec7fae
Ksusha Zhukova <zhukovaksusha@gmail.com>
2018-02-02 17:13:10 +0300

    update logic, update var names

--- check_sites_health.py
+++ check_sites_health.py
@@ -4,72 +4,52 @@ from datetime import datetime
 import whois
 
 
-def collect_urls_to_a_list(text_file):
-    list_of_links = []
+def load_urls_to_a_list(text_file):
     with open(text_file) as file_of_links:
-        for link in file_of_links:
-            list_of_links.append(link.strip())
-    return list_of_links
+        return [(*file_of_links)]
 
 
-def check_server_status_code(url):
+def is_server_respond_ok(domain):
     try:
-        website_header = requests.head(url)
-        return website_header.status_code
+        response = requests.request('GET', domain)
+        return response.ok
     except requests.ConnectionError:
         return None
 
 
 def get_domain_expiration_date(url):
-    website_info = whois.whois(url)
-    if website_info.expiration_date is None and website_info.status is None:
-        return None
-    elif type(website_info.expiration_date) == list:
-        website_info.expiration_date = website_info.expiration_date[0]
-    else:
-        website_info.expiration_date = website_info.expiration_date
-
-    return website_info.expiration_date
-
-
-def output_status_and_exp_date_to_console(todays_date,
-                                          ok_status_code,
-                                          minimum_days_of_domain_payment,
-                                          server_respond,
-                                          exp_date):
-    if server_respond == ok_status_code:
-        print("HTTP Status Code 200: The request has succeeded", end=" ")
-    else:
-        print("HTTP Status Code is {}".format(server_respond), end=" ")
-
-    if exp_date is None:
-        print('The domain does not exist, exiting...')
-    else:
-        days_due_expirency = abs(exp_date - todays_date).days
-        if days_due_expirency <= minimum_days_of_domain_payment:
-            print("Domain expires in {} days".format(days_due_expirency))
+    try:
+        domain_info = whois.whois(url)
+        if type(domain_info.expiration_date) == list:
+            return domain_info.expiration_date[0]
         else:
-            print("Domain lasts more than 30 days")
+            return domain_info.expiration_date
+    except whois.parser.PywhoisError:
+        exit("No domain match for {}".format(url))
+
+
+def check_if_expir_in_30_d(exp_date, todays_date):
+    min_days_of_payment = 30
+    days_due_expirency = abs(exp_date - todays_date).days
+    if days_due_expirency <= min_days_of_payment:
+        return True
+    return False
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        text_file = sys.argv[1]
-        todays_date = datetime.now()
-        links = collect_urls_to_a_list(text_file)
-        ok_status_code = 200
-        minimum_days_of_domain_payment = 30
-        for link in links:
-            print(link, end=" ")
-            server_respond = check_server_status_code(link)
-            if server_respond is None:
-                exit("failed to connect")
-            exp_date = get_domain_expiration_date(link)
-            output_status_and_exp_date_to_console(todays_date,
-                                                  ok_status_code,
-                                                  minimum_days_of_domain_payment,
-                                                  server_respond,
-                                                  exp_date)
+    if len(sys.argv) == 1:
+        exit("Enter a valid txt file")
+    text_file = sys.argv[1]
+    links = load_urls_to_a_list(text_file)
+    todays_date = datetime.now()
 
-    else:
-        print("Enter a valid txt file")
+    for link in links:
+        print("Checking {}".format(link.strip()))
+        if is_server_respond_ok(link):
+            print("HTTP Status Code: OK")
+        else:
+            print("HTTP Status Code: Not OK")
+
+        exp_date = get_domain_expiration_date(link)
+        expires_in_30_days = check_if_expir_in_30_d(exp_date, todays_date)
+        print("Domain expires in 30 days: {}".format(expires_in_30_days))
